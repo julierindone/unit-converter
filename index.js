7ea2a43451625827a568e89dd3e498f336671fa1
Julie Rindone <julie.rindone@gmail.com>
2023-10-27 15:59:24 -0500

    Added some input validation and broke printToScreen

--- index.js
+++ index.js
@@ -6,23 +6,41 @@ const massEl = document.getElementById("mass-el")
 
 convertbtn.addEventListener("click", function () {
   // call calculateConversions with the input passed in as arg
-     // console.log(`value to convert: ${valueToConvert.value}`)
-  inputNum = Number(valueToConvert.value)
-  calculateConversions(inputNum)
+  console.log(`value to convert: ${valueToConvert.value}`)
+  validateInput()
 })
 
-function calculateConversions(num) {
-  console.log(num)
+function validateInput() {
+  console.log(`validate Input ${valueToConvert.value} and length is ${valueToConvert.value.length}`)
+  if (valueToConvert.value.length > 2) {
+    console.log(`${valueToConvert}'s length Is Greater than 2`)
+    alert("Your number is too large!");
+    valueToConvert.value = " "
+    return false;
+  }
+  
+  else {
+    
+    let inputNum = Number(valueToConvert.value)
+    console.log(`inputNum is ${inputNum}. Returning input now.`)
+    calculateConversions(inputNum)
+  }
+}
+function calculateConversions(inputNum) {
+  console.log(`inside the calcconversions, inputnumber is  ${inputNum}`)
 
-  // each variable holds an array 
-    // [0] = imp to met, [1] = met to imp
-  let length = [(num * 0.3048).toFixed(3), (num / 0.3048).toFixed(3)]
-  let volume = [(num * 3.785).toFixed(3), (num / 3.785).toFixed(3)]
-  let mass = [(num * 0.45359237).toFixed(3), (num / 0.45359237).toFixed(3)]
+// //  each variable holds an array 
+  // [0] = imp to met, [1] = met to imp
+  let length = [(inputNum * 0.3048).toFixed(3), (inputNum / 0.3048).toFixed(3)]
+  let volume = [(inputNum * 3.785).toFixed(3), (inputNum / 3.785).toFixed(3)]
+  let mass = [(inputNum * 0.45359237).toFixed(3), (inputNum / 0.45359237).toFixed(3)]
+
+  console.log(length  + volume + mass)
 
   printToScreen(length, volume, mass)
 }
 
+// WHY DOESNT THIS FUCKING WORK
 function printToScreen(length, volume, mass) {
   // send to screen with innerHTML 
   lengthEl.innerHTML = `<p class="output">${inputNum} feet = ${length[0]} meters
@@ -34,13 +52,3 @@ function printToScreen(length, volume, mass) {
   massEl.innerHTML = `<p class="output">${inputNum} pounds = ${mass[0]} kilograms
   <br />${inputNum} kilograms = ${mass[1]} pounds</p>`
 }
-
-//Got this from Codepen but I don't really know if it's needed... my input thing doesn't work either way.
-// Array.from( document.querySelectorAll('[data-expand]'), (input)=>{
-//   let parent = input.parentNode;
-//   function updateSize(){
-//     parent.dataset.value = input.value
-//   }
-//   input.addEventListener('input', updateSize);
-//   updateSize();
-// });
